f8e1f13f53efab7cc6e754df1c6aebac74d9c848
ashish <ba1089@srmist.edu.in>
2026-03-11 15:10:55 +0530

    Added UC7 CharacterPattern class implementation

---
+++ UC7.java
@@ -0,0 +1,78 @@
+import java.util.*;
+
+public class UC7 {
+
+    static class CharacterPattern {
+
+        private char character;
+        private String[] pattern;
+
+        public CharacterPattern(char character, String[] pattern) {
+            this.character = character;
+            this.pattern = pattern;
+        }
+
+        public char getCharacter() {
+            return character;
+        }
+
+        public String[] getPattern() {
+            return pattern;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Map<Character, CharacterPattern> patternMap = new HashMap<>();
+
+        patternMap.put('O', new CharacterPattern('O', new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        }));
+
+        patternMap.put('P', new CharacterPattern('P', new String[]{
+                "****** ",
+                "*     *",
+                "*     *",
+                "****** ",
+                "*      ",
+                "*      ",
+                "*      "
+        }));
+
+        patternMap.put('S', new CharacterPattern('S', new String[]{
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        }));
+
+        String word = "OOPS";
+
+        int height = 7;
+
+        for (int i = 0; i < height; i++) {
+
+            StringBuilder line = new StringBuilder();
+
+            for (char c : word.toCharArray()) {
+
+                CharacterPattern cp = patternMap.get(c);
+
+                if (cp != null) {
+                    line.append(cp.getPattern()[i]).append("  ");
+                }
+            }
+
+            System.out.println(line);
+        }
+    }
+}
